2b440024b0ee8d4657c22075c4248d266f78b73d
Mohamed Aimane Skhairi <skhairimedaimane@gmail.com>
2019-09-21 02:42:34 +0100

    ALC4P2 Function C3

--- funWithFunctions/challenge1.js
+++ funWithFunctions/challenge1.js
@@ -2,6 +2,7 @@
 // utils
 const log = arg => console.log(arg);
 
+// Function Challenge 1
 // 1 - binary functions
 // sum
 const add = (a, b) => a + b;
@@ -39,6 +40,7 @@ log(liftf(mul)(5)(6)); // 30
 // Curry
 const curry = (binaryFns, a) => (b) => binaryFns(a, b);
 const curry2 = (binaryFns, a) => liftf(binaryFns)(a);
+const curry3 = (func, ...args) => (b) => func(...args, b); // generic way
 const add3 = curry(add, 3);
 log(add3(4)); // 7
 log(curry(mul, 5)(6)); // 30
@@ -51,3 +53,39 @@ const inc1 = liftf(add)(1);
 const inc3 = addf(1);
 log(inc(5)); // 6
 log(inc(inc(5))); // 7
+
+// Function Challenge 3
+// twice
+const twice = binaryFns => a => binaryFns(a, a);
+const doubl = twice(add);
+const square = twice(mul);
+log(add(11, 11)); // 22
+log(doubl(11)); // 22
+log(square(11)); // 121
+
+// function that call nested fuction with a reversed args.
+// reverses
+const reverse = binaryFns => (a, b) => binaryFns(b, a);
+const reverse2 = func => (...args) => func(...args.reverse()); // generic way
+const bus = reverse(sub);
+log(bus(3, 2)); // -1
+
+// composeu - takes two unary funcs and returns one unary function that calls them both
+const composeu = (unaryFns1, unaryFns2) => arg => unaryFns2(unaryFns1(arg));
+log(composeu(doubl, square)(5)); // 100
+
+// composeb - takes two binary funcs and returns func that calls them both
+const composeb = (binaryFns1, binaryFns2) => (a, b, c) => binaryFns2(binaryFns1(a, b), c);
+log(composeb(add, mul)(2, 3, 7)); // 35
+
+// limit - allows a binary function to be called a limited number of times (useful for third party endpoint)
+const limit = (binaryFns, limitedValue) => (a, b) => {
+  if(limitedValue > 0) { 
+    limitedValue -= 1;
+    return binaryFns(a, b);
+  }
+  return undefined;
+}
+const addLtd = limit(add, 1);
+log(addLtd(3, 4)); // 7
+log(addLtd(3, 5)); // undefined
